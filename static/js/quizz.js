17b0dba279548d62f855eab78577dad3d20869fe
kapil <kbchonde@gmail.com>
2020-07-17 07:01:07 +0530

    my online portal

---
+++ static/js/quizz.js
@@ -0,0 +1,6 @@
+var answers = ["A","C","B"],
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); // Get radio group by-name
+    for(var y=0; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; // return the checked value
+}
